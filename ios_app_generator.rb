7dda5d744c296a88219806e90615063dda75609c
Seb <sebweston@Sebs-MacBook-Air.local>
2012-08-16 18:38:31 +1000

    bit more

--- ios_app_generator.rb
+++ ios_app_generator.rb
@@ -1,3 +1,15 @@
-class IosAppGenerator < Rails::Generators::NamedBase
+class IosAppGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
+
+  argument :model_name, :type => :string
+
+ 	def generate_controller
+ 		template 'ViewController', "xcode_project/controllers/#{file_name}ViewController.h"
+  end
+
+  private
+
+  def file_name
+  	model_name
+  end
 end
---
+++ templates/ViewController.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+@interface PostsViewController : UITableViewController
+{
+	NSMutableArray *posts;
+}
+
+@end
